7bf4a6b29358c6561ac915dae60e4e6e90a17991
emanmkhareez <emkhareez19@gmail.com>
2021-08-25 15:19:34 -0700

    add

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const express = require("express");
+const cors=require('cors')
 const app = express();
+app.use(cors())
+
 app.use(express.json());
 const userRoute = require("./router/user");
 const start = (port) => {
